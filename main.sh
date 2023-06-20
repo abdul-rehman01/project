b7db11c22abc908cb43ff0405fb4c1fab9b9a20d
abdul-rehman01 <130767404+abdul-rehman01@users.noreply.github.com>
2023-06-20 19:06:15 +0500

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,25 @@
+#!bin/bash
+
+PASSWORD="123456"
+
+
+file_content=$(cat /home/abdul/project/main.txt)
+
+
+if [[ $file_content == *development*  ]]; then
+
+echo $PASSWORD | sudo -S apt update -y
+echo $PASSWORD | sudo -S apt upgrade -y
+
+echo $PASSWORD | sudo -S apt-get install apache2 -y
+
+echo $PASSWORD | sudo -S  apt-get install mysql-server -y
+
+echo $PASSWORD | sudo -S  apt-get install php libapache2-mod-php php-mysql -y
+
+
+else
+
+echo $password | sudo -S mkdir -p /home/abdul/master 
+
+fi
